a1daa2ef46f146120065e072962c6b0fe57795ca
coffeeandcodee <a9idris@gmail.com>
2024-06-11 11:21:08 +0100

    Added getWeatherData

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const apikey = "3345e73d17300187cf5b707ec0eaeb71";
 
 //Refers to the div with the Id of "weather-data", i.e the large div with all the data in it//
-const weatherDataElement = document.getElementById("weather-data");
-const inputEl = document.getElementById("city-input");
+const weatherDataEl = document.getElementById("weather-data");
 const cityInputEl = document.getElementById("city-input");
 
 //Using a querySelector as the form doesn't have an Id value
-const formEl = document.querySelector("form");
+const form = document.querySelector("form");
 
-formEl.addEventListener("submit", (event) => {
-  //Default behaviour of submit button is to refresh the page. Line below prevents this default
-  //behaviour
+//Default behaviour of submit button is to refresh the page. Line below prevents this default
+//behaviour
+
+form.addEventListener("submit", (event) => {
   event.preventDefault();
   const cityValue = cityInputEl.value;
   console.log(cityValue);
+  getWeatherData(cityValue);
 });
+
+function getWeatherData(value) {
+  try {
+  } catch (error) {}
+}
