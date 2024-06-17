99224716561243461782091ba43c03d08cc704f5
coffeeandcodee <a9idris@gmail.com>
2024-06-17 13:34:59 +0100

    basic version of calculator complete

--- basic-calculator/script.js
+++ basic-calculator/script.js
@@ -1,3 +1,28 @@
 const buttonsEl = document.querySelectorAll("button");
+const inputElField = document.getElementById("result");
 
-console.log(buttonsEl);
+for (let i = 0; i < buttonsEl.length; i++) {
+  buttonsEl[i].addEventListener("click", () => {
+    const buttonValue = buttonsEl[i].textContent;
+    if (buttonValue === "C") {
+      clearResult();
+    } else if (/[0-9, +, -, /, *]/.test(buttonValue)) {
+      appendValue(buttonValue);
+    } else if (buttonValue == "=") {
+      calculateResult();
+    }
+  });
+}
+
+function clearResult() {
+  inputElField.value = "";
+}
+
+function appendValue(value) {
+  // made the mistake of inputElField += value initially
+  inputElField.value += value;
+}
+
+function calculateResult() {
+  inputElField.value = eval(inputElField.value);
+}
